90343e7259d48622b007cc7eefe2022fb948921f
Peter Iskandar <piskandar@pivotal.io>
2018-01-19 15:46:11 -0500

    screipt
    Signed-off-by: David Nowak <dnowak@pivotal.io>

--- ci/tasks/deploy/deploy.sh
+++ ci/tasks/deploy/deploy.sh
@@ -4,4 +4,4 @@ set -e -x
 
 cf login -a http://api.run.pivotal.io -u $CF_USERNAME -p $CF_PASSWORD -o $CF_ORG -s $CF_SPACE
 
-cf push release-test gh-release/release-test-0.0.1-SNAPSHOT.jar
+cf push release-test -p gh-release/release-test-0.0.1-SNAPSHOT.jar
