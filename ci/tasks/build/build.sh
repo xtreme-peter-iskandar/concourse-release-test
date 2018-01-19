d2d084864cc80e74374d933cbd6660c052ca3483
Peter Iskandar <piskandar@pivotal.io>
2018-01-19 13:59:41 -0500

    added pipeline
    Signed-off-by: David Nowak <dnowak@pivotal.io>

--- ci/tasks/build/build.sh
+++ ci/tasks/build/build.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 set -e -x
 
+./gradlew clean build
+
+time=$(date +%s)
+
+date=$(date +%Y-%m-%d)
+
+mkdir releases
+
+echo -n "$time" > releases/tag
+echo -n "release $time" > releases/name
+
